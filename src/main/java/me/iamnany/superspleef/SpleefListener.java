6fd2bc541b6138241f5cad578c2e58bef797eeca
Michal Valko <valko.miso111@gmail.com>
2024-09-05 12:05:21 +0200

    added snowballs

--- src/main/java/me/iamnany/superspleef/SpleefListener.java
+++ src/main/java/me/iamnany/superspleef/SpleefListener.java
@@ -1,12 +1,16 @@
 package me.iamnany.superspleef;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -15,7 +19,7 @@ import java.util.UUID;
 
 public class SpleefListener implements Listener {
     private HashMap<UUID, Long> lastInteractionTime = new HashMap<>();
-    private final long cooldownTime = 100; // Cooldown time in milliseconds (e.g., 1000ms = 1 second)
+    private final long cooldownTime = 10; // Cooldown time in milliseconds (e.g., 1000ms = 1 second)
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
@@ -36,8 +40,27 @@ public class SpleefListener implements Listener {
 
             }
                 lastInteractionTime.put(playerId, currentTime);
+
+                //giving player snowballs after they break a block
+                if (!player.getInventory().contains(Material.SNOWBALL, 16)) {
+                    ItemStack itemStack = new ItemStack(Material.SNOWBALL, 1);
+                    player.getInventory().addItem(itemStack);
+                }
+
+
+
                 event.getClickedBlock().setType(Material.AIR);
                 event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        Projectile projectile = event.getEntity();
+        if (projectile.getType() == EntityType.SNOWBALL) {
+            if (event.getHitBlock() != null) {
+                event.getHitBlock().setType(Material.AIR);
             }
         }
     }
